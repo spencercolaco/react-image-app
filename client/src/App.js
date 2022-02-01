0cb13c62e6f137095ade813566f9ec4079df2438
spencercolaco <spencercola@gmail.com>
2022-02-01 15:25:59 -0600

    setups up API with express and client side with react

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Typography } from "@material-ui/core";
+import UploadImages from "./components/upload-images.component";
+
+function App() {
+  return (
+    <div className="container">
+      
+    </div>
+  );
+}
+
+export default App;
