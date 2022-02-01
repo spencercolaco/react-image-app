37bda67a9831d46663a79acbb026d7fe2957fbbd
spencercolaco <spencercola@gmail.com>
2022-02-01 17:49:46 -0600

    fixes front-end bugs causing files to not store through multer

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,22 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
-  const [image, setImage] = useState([{ preview: '', data: ''}])
+  // 2. Create our *list* variable as well as the *setList* function via useState
+  // We're setting the default value of list to null, so that while we wait for the
+  // fetch to complete, we dont attempt to render the list
+  const [list, setList] = useState(null)
+  const [image, setImage] = useState({ preview: '', data: '' })
   const [status, setStatus] = useState('')
+
+  // 3. Create out useEffect function
+  useEffect(() => {
+    fetch("http://localhost:5000/image/")
+    .then(response => response.json())
+        // 4. Setting *list* to the list url that we received from the response above
+    .then(data => setList(data))
+    .catch((error) => console.log(error));
+  },[])
+
   const handleSubmit = async (e) => {
     e.preventDefault()
     let formData = new FormData()
@@ -15,25 +29,16 @@ function App() {
     if (response) setStatus(response.statusText)
   }
 
-  // useEffect(() => {
-  //   fetch("http://localhost:5000/image")
-  //   .then(response => response.json())
-  //       // 4. Setting *dogImage* to the image url that we received from the response above
-  //   .then(data => setImage(data))
-  // },[])
-
   const handleFileChange = (e) => {
     const img = {
       preview: URL.createObjectURL(e.target.files[0]),
       data: e.target.files[0],
     }
     setImage(img)
-    // console.log(img)
   }
-  console.log(image)
 
   return (
-    <div className='App'>
+    <div className="App">
       <h1>Upload to server</h1>
       {image.preview && <img src={image.preview} width='100' height='100' />}
       <hr></hr>
@@ -42,9 +47,12 @@ function App() {
         <button type='submit'>Submit</button>
       </form>
       {status && <h4>{status}</h4>}
-      {/* {image && image.map((img) => <img width={"200px"} height={"200px"} src={img.url}></img>)} */}
+    {/* 5. Returning an img element for each url, again with the value of our src set to the list url */}
+    {list &&
+      list.map((img, i) => <img key={img.name} width={"200px"} src={img.url} />)
+    }
     </div>
-  )
+  );
 }
 
 export default App;
